c8d5aef045ce7df522f639db1743749f161108ac
rgarciah <rgarciah@ext.laligatech.com>
2023-11-17 17:23:15 +0100

    Added find route by key

--- src/cms/page.query.ts
+++ src/cms/page.query.ts
@@ -10,8 +10,8 @@ interface IRoutePageGeneratorParams {
 type AsyncFunction = (props: IRoutePageGeneratorParams) => Promise<Page<PageContent>>;
 type IPageResolver<T extends Key> = Record<T, AsyncFunction>;
 
-type Key = string | number | symbol;
-type IRoutes<T extends Key> = Record<T, string>;
+export type Key = string | number | symbol;
+export type IRoutes<T extends Key> = Record<T, string>;
 
 export type RoutesDefinitions<T extends Key> = {
     [locale: string]: IRoutes<T>;
@@ -24,35 +24,34 @@ export type AsyncPageRoutes<T> = {
 
 export const buildPageLoader = <T extends Key>(routesDefinition: RoutesDefinitions<T>, pageByRoute: IPageResolver<T>) => {
   return async (request: Request, lang: string): Promise<Optional<Page<PageContent>>> => {
-    const currentPath = new URL(request.url).pathname;
-    const key = findRouteKey<T>(routesDefinition, request);
-    if (!key) return undefined;
+      const pathname = new URL(request.url).pathname;
+      const key = findRouteKey<T>(routesDefinition.default, pathname);
+      if (!key) return undefined;
 
-    const routesDictionary = routesDefinition[lang] ?? routesDefinition.default;
+      const routesDictionary = routesDefinition[lang] ?? routesDefinition.default;
 
-    const foundRoutes = Object.entries(routesDictionary).flatMap(([key, routePattern]) => {
-      const matcher = match(routePattern as string, { decode: decodeURIComponent });
-      const matchResult = matcher(currentPath);
-      return matchResult ? [{ key, params: matchResult.params }] : [];
-    });
-    const foundRoute = foundRoutes[0];
+      const foundRoutes = Object.entries(routesDictionary).flatMap(([key, routePattern]) => {
+          const matcher = match(routePattern as string, { decode: decodeURIComponent });
+          const matchResult = matcher(pathname);
+          return matchResult ? [{ key, params: matchResult.params }] : [];
+      });
+      const foundRoute = foundRoutes[0];
 
-    if (!foundRoute) return undefined;
+      if (!foundRoute) return undefined;
 
-    const pageParams = { lang, ...foundRoute.params };
-    return pageByRoute[key](pageParams);
+      const pageParams = { lang, ...foundRoute.params };
+      return pageByRoute[key](pageParams);
   };
 }
 
-export const findRouteKey = <T extends Key>(routesDefinition: RoutesDefinitions<T>, request: Request): T | undefined => {
-  const currentPath = new URL(request.url).pathname;
-  const routesDictionary = routesDefinition.default;
+export const findRouteKey = <T extends Key>(routes: IRoutes<T>, pathname: string): T | undefined => {
 
-  for (const [key, routePattern] of Object.entries(routesDictionary)) {
-    const matcher = match(routePattern as string, { decode: decodeURIComponent });
-    if (matcher(currentPath)) {
-      return key as T;
-    }
+  for (const [key, routePattern] of Object.entries(routes)) {
+      const matcher = match(routePattern as string, { decode: decodeURIComponent });
+      if (matcher(pathname)) {
+          return key as T;
+      }
   }
   return undefined;
 };
+
