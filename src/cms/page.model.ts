06052ff44e9827dc360e4c25d16bb1929e99a704
Fiser <ruben@nexolabs.xyz>
2025-06-23 00:46:16 +0200

    feat: updated to add unique ID to the page to get the renderer

--- src/cms/page.model.ts
+++ src/cms/page.model.ts
@@ -2,10 +2,11 @@ import { Optional } from "../core/optional.type";
 import { ComponentUtilitiesSeo } from "./meta.model";
 
 export interface PageContent {  
-
+    __typename: string;
 }
   
 export type Page<T extends PageContent> = {
+    pageId?: string;
     title: String;
     content: Array<T>;
     SEO: Optional<ComponentUtilitiesSeo>;
